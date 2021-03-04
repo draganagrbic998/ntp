1980c40664cb8f0892eb6476443dcc89b4e16467
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-05 00:24:10 +0100

    testirala users mikroservis

--- user_service/users/tests.py
+++ user_service/users/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class ActivationTest(TestCase):
+
+    def test_activation(self):
+        c = Client()
+        response = c.get('/activate/asd')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/activation.html')
