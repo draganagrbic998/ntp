a79c2f54a8da6a49419b734f78396d5b438edd62
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-06 18:38:57 +0100

    preimenovala bat file

--- launch.bat
+++ launch.bat
@@ -1,4 +1,6 @@
+venv\scripts\python user_service/manage.py migrate 
 start venv\scripts\python user_service/manage.py runserver 
+venv\scripts\python comment_service/manage.py migrate
 start venv\scripts\python comment_service/manage.py runserver 8003
 cd ad_service
 set GO111MODULE=auto
