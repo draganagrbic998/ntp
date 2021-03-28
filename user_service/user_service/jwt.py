a91b7d6822ceda2f9ce22bcdb640a82ecb5fadd5
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-28 14:23:50 +0200

    simple

--- user_service/user_service/jwt.py
+++ user_service/user_service/jwt.py
@@ -5,7 +5,7 @@ def jwt_response_handler(token, user=None, request=None):
     try:
         custom_user = CustomUser.objects.get(user_id=user.id)
         return {
-            'id': custom_user.id,
+            'id': custom_user.user.id,
             'token': token,
             'email': custom_user.user.email,
             'first_name': custom_user.user.first_name,
